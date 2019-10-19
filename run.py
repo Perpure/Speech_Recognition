828e96e7ce1c9f20f6bd2b181aaaba00014c4b91
Paul Dyachek <dyachekp@gmail.com>
2019-10-19 09:44:10 +0300

    Adapting run.py for console

--- run.py
+++ run.py
@@ -1,6 +1,26 @@
 import speech_recognition as sr
+import json
+
+print("Working...")
 r = sr.Recognizer()
-interview = sr.AudioFile("Interview.wav")
-with interview as source:
-    audio = r.record(source)
-print(r.recognize_google(audio, language='ru-RU'))
+try:
+    interview = sr.AudioFile("Interview.wav")
+    with interview as source:
+        audio = r.record(source)
+except FileNotFoundError:
+    print("Put \"Interview.wav\" in this directory!")
+    input()
+else:
+    file = open("Interview.txt", 'w')
+    try:
+        file.write(r.recognize_google(audio, language='ru-RU'))
+        file.close()
+    except json.decoder.JSONDecodeError:
+        print("Your request is empty or the Internet connection lost")
+        input()
+    except sr.RequestError:
+        print("Request error. Probably your file is too large")
+        input()
+    else:
+        print("Success!")
+        input()
